5e8d8a71e1ef254397f68ac610ed97d274281c87
Quang Nguyen <quangctkm9207@gmail.com>
2020-02-04 16:58:13 +0900

    Update basic-implementation.m

--- basic-implementation.m
+++ basic-implementation.m
@@ -1,19 +1,16 @@
 % Basic machine learning functions represented in Octave
 
-% Notation
+% NOTATION
 % m : the number of training samples
 % n : the number of features (inputs)
 % X : the input of training samples ([m x (n + 1)] matrix)
 % y : the output of training samples ([m x 1] vector)
 % theta : the fitting parameters ([(n + 1) x 1] vector)
 
-% Cost function
-m = length(y); % number of training examples
-
-function J = costFunction(X, y, theta)
+% COST FUNCTION
 J = (1/(2*m)) * sum((X*theta - y).^2)
 
-% Gradient Descent
+% GRADIENT DESCENT
 J_history = zeros(num_iters, 1);
 
 for i = 1:iterations
@@ -22,3 +19,12 @@ for i = 1:iterations
   % Save the cost J in every iteration
   J_history(iter) = costFunction(X, y, theta);
 end
+
+% FEATURE NORMALIZATION
+X_norm = (X .- mean(X,1)) ./ std(X,0,1);
+
+% NORMAL EQUATION
+theta = (X'*X)^(-1)*X'*y;
+
+% PREDICTION
+value = theta' * newInput
