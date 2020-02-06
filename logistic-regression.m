37d3eb671b1ecdbee94c6a4033c93f3f52724577
Quang Nguyen <quangctkm9207@gmail.com>
2020-02-06 12:05:18 +0900

    Add regularized logistic regression's implementation

--- logistic-regression.m
+++ logistic-regression.m
@@ -14,3 +14,12 @@ grad = (1/m) * X' * (h - y);
 
 % PREDICT FUNCTION (p)
 p = h >= 0.5;
+
+% SPECIAL: FOR REGULARIZED LOGISTIC REGRESSION
+% It includes lambda.
+% Other functions are the same, only COST and GRADIENT functions are different.
+shift_theta = theta(2:size(theta)); % Avoid regularize theta 1
+theta_reg = [0;shift_theta];
+
+J = (1/m) * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + (lambda / (2 * m)) * sum(theta_reg.^2);
+grad = (1/m) * X' * (h - y) + (lambda/m) * theta_reg;
